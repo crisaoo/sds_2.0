84ee84e9ba3bdf255689503976520fe940385c77
Crisaoo <Cristianocosta2019@outlook.com>
2021-01-05 23:11:59 -0300

    Post Order

--- backend/src/main/java/com/pescaria/dsdelivery/controllers/OrderController.java
+++ backend/src/main/java/com/pescaria/dsdelivery/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.pescaria.dsdelivery.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pescaria.dsdelivery.dto.OrderDTO;
 import com.pescaria.dsdelivery.services.OrderService;
@@ -21,5 +25,12 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll(){
 		List <OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
--- backend/src/main/java/com/pescaria/dsdelivery/services/OrderService.java
+++ backend/src/main/java/com/pescaria/dsdelivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.pescaria.dsdelivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,19 +8,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.pescaria.dsdelivery.dto.OrderDTO;
+import com.pescaria.dsdelivery.dto.ProductDTO;
 import com.pescaria.dsdelivery.entities.Order;
 import com.pescaria.dsdelivery.entities.OrderStatus;
+import com.pescaria.dsdelivery.entities.Product;
 import com.pescaria.dsdelivery.repositories.OrderRepository;
-import com.pescaria.dsdelivery.dto.OrderDTO;
+import com.pescaria.dsdelivery.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 	@Autowired
 	private OrderRepository repository;
+	@Autowired
+	private ProductRepository prodRepository;
 	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findAllPendingOrderOrderByMomentAsc();
 		return list.stream().map(o -> new OrderDTO(o)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert (OrderDTO objDTO) {
+		Order obj = new Order(null, objDTO.getAddress(), objDTO.getLatitude(), objDTO.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		
+		for (ProductDTO pDTO : objDTO.getProducts()) {
+			Product p = prodRepository.getOne(pDTO.getId());
+			obj.getProducts().add(p);
+		}
+		
+		return new OrderDTO(repository.save(obj));
 	}
 }
