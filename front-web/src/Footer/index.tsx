84451e587add052fa9cdc30ae30bd644b8dd392a
Crisaoo <Cristianocosta2019@outlook.com>
2021-01-07 22:42:35 -0300

    Adding footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,26 @@
+import './styles.css';
+import {ReactComponent as InstagramIcon} from './instagram.svg';
+import {ReactComponent as LinkedinIcon} from './linkedin.svg';
+import {ReactComponent as GithubIcon} from './github.svg';
+
+function Footer () {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+
+            <div className="footer-icons">
+                <a href="linkedin" target="_new">
+                    <LinkedinIcon />
+                </a>
+                <a href="https://www.instagram.com/crisaocost" target="_new">
+                    <InstagramIcon />
+                </a>
+                <a href="https://www.github.com/crisaoo" target="_new">
+                    <GithubIcon />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
