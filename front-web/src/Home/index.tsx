a058920a09c69368e895e1b0dbaed441993969a3
Crisaoo <Cristianocosta2019@outlook.com>
2021-01-09 15:29:53 -0300

    Search order location

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,5 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
-import Footer from '../Footer';
 import { Link } from 'react-router-dom';
 
 function Home() {
@@ -27,7 +26,6 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <Footer />
         </>
     )
 }
