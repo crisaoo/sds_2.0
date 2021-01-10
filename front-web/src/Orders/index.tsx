98755ac8d95c040089a4ff31e2fc5137b8c11fb9
Crisaoo <Cristianocosta2019@outlook.com>
2021-01-10 10:58:01 -0300

    Revert "Add env vars"
    This reverts commit 0fd10a6276c0d5ec345d07b80772ad04b9facc2c.

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -22,7 +22,7 @@ function Orders() {
     useEffect(() =>{
         fetchProducts()
         .then(response => setProducts(response.data))
-        .catch(() => toast.warning('Erro ao listar produtos'))
+        .catch(error => console.log(error))
     }, [])
 
     const handleSelectProduct = (product: Product) => {
